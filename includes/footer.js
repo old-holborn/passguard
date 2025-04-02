2ee719d7e9458593c84e8f709cbe9809f86b2c3e
Old Holborn <anta777@keemail.me>
2025-04-02 20:39:44 +0300

    Add files via upload

---
+++ includes/footer.js
@@ -0,0 +1,19 @@
+document.getElementById("footer-container").innerHTML = `  
+<center>
+    <footer>
+        <h1>PassGuard is under active development.</h1>
+
+        <form action=" ">
+            <input type="text" id="email" placeholder="Email"><br>
+            <input type="submit" value="Apply">
+        </form>
+
+        <p>Made with <span>♥</span> by Old Holborn</p>
+        <ul class="footer-links">
+            <li><a href="/legal">Legal</a></li>
+            <li><a href="/privacy">Privacy</a></li>
+            <li><a href="/privacy#terms">Terms</a></li>
+        </ul>
+    </footer>
+</center>
+`;
